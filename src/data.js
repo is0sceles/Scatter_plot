d62c3e77ecbc15921194f92b5d67fd5d6cc19ed1
Wasiff <wasiff@gmail.com>
2017-12-07 10:16:02 -0800

    add data stubs

--- src/data.js
+++ src/data.js
@@ -23,6 +23,62 @@ const plotpoints = [
     start_time: '2017-11-28T14:12:12Z',
     status: 'pass',
     duration: 200,
+  }, {
+    start_time: '2017-11-28T04:06:12Z',
+    status: 'pass',
+    duration: 106, // in seconds
+  }, {
+    start_time: '2017-11-28T01:22:12Z',
+    status: 'error',
+    duration: 300,
+  }, {
+    start_time: '2017-11-28T03:24:12Z',
+    status: 'fail',
+    duration: 120,
+  }, {
+    start_time: '2017-11-28T03:24:12Z',
+    status: 'pass',
+    duration: 290,
+  }, {
+    start_time: '2017-11-29T06:24:12Z',
+    status: 'error',
+    duration: 290,
+  }, {
+    start_time: '2017-11-28T11:12:12Z',
+    status: 'pass',
+    duration: 230,
+  }, {
+    start_time: '2017-11-28T19:06:12Z',
+    status: 'pass',
+    duration: 106, // in seconds
+  }, {
+    start_time: '2017-11-28T20:22:12Z',
+    status: 'error',
+    duration: 199,
+  }, {
+    start_time: '2017-11-28T21:24:12Z',
+    status: 'fail',
+    duration: 120,
+  }, {
+    start_time: '2017-11-28T23:24:12Z',
+    status: 'pass',
+    duration: 290,
+  }, {
+    start_time: '2017-11-28T19:24:12Z',
+    status: 'error',
+    duration: 290,
+  }, {
+    start_time: '2017-11-28T19:12:12Z',
+    status: 'pass',
+    duration: 230,
+  }, {
+    start_time: '2017-11-28T22:06:12Z',
+    status: 'pass',
+    duration: 106, // in seconds
+  }, {
+    start_time: '2017-11-28T21:22:12Z',
+    status: 'error',
+    duration: 199,
   },
 
 ];
